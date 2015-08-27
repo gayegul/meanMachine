42dc00e9da80d003b47c0328356eeafb0315d836
Gaye Bulut <gayegul@gmail.com>
2015-08-26 22:15:49 -0700

    add the admin router

--- awesome-test/server.js
+++ awesome-test/server.js
@@ -1,10 +1,25 @@
 var express = require('express'),
     app = express(),
+    adminRouter = express.Router(),
     path = require('path');
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
+
+adminRouter.get('/', function(req, res) {
+  res.send('I am the dashboard!');
+});
+
+adminRouter.get('/users', function(req, res) {
+  res.send('I show all the users!');
+});
+
+adminRouter.get('/posts', function(req, res) {
+  res.send('I show all the posts!');
+});
+
+app.use('/admin', adminRouter);
 
 app.listen(1337);
 console.log('1337 is the magic port!');
