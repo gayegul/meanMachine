c4d09133d911f93e6e90ceaec66dcfb8b084ac25
Gaye Bulut <gayegul@gmail.com>
2015-08-31 11:07:06 -0700

    add server

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var mongoose = require('mongoose');
+var port = process.env.PORT || 8080;
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  res.setHeader('Access-Control-Allow-Headers', 'x-Requested-With, content-type, \ Authorization');
+  next();
+});
+
+app.use(morgan('dev'));
+
+app.get('/', function(req, res) {
+  res.send('Welcome to the home page!');
+});
+
+var apiRouter = express.Router();
+
+apiRouter.get('/', function(req, res) {
+  res.json({message: 'horray! welcome to our api!'});
+});
+
+app.use('/api', apiRouter);
+
+app.listen(port);
+console.log('Magic happens on port ' + port);
